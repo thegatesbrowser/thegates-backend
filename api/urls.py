25591f9adc64b0a02ba4549b96ee492b83a6ab81
Nordup <nordup.ondr@gmail.com>
2023-06-22 02:44:08 +0400

    add gate

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, re_path
 from . import views
 from . import search
+from . import add_gate
 
 
 urlpatterns = [
     path('api/analytics_event', views.analytics_event),
     path('api/get_user_id', views.get_user_id),
-    path('api/search', search.search)
+    path('api/search', search.search),
+    path('api/add_gate', add_gate.add_gate)
 ]
