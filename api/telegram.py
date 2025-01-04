5a90eb0d6f5076fac9723df9aae1abcced33cd9b
Nordup <nordup.ondr@gmail.com>
2025-01-04 09:22:19 +0400

    print city in bot

--- api/telegram.py
+++ api/telegram.py
@@ -6,17 +6,33 @@ chat_id = open('keys/my_telegram_id.key', 'r').read()
 url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
 
 
+def get_location(ip_address: str):
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {
+        'ip': ip_address,
+        'city': response.get('city'),
+        'region': response.get('region'),
+        'country': response.get('country_name')
+    }
+    return location_data
+
+
 def bot_notify_event(data):
-    user_id = data.pop('user_id')
-    event_name = data.pop('event_name')
+    user_id = data.get('user_id')
+    event_name = data.get('event_name')
     user_ignore = open('staticfiles/telegram_bot_user_ignore.txt', 'r').read()
     
-    if event_name != "application_open" or user_id in user_ignore:
+    if event_name != 'application_open' or user_id in user_ignore:
         return
+    
+    ip = data.get('ip')
+    location_data = get_location(ip)
+    city = location_data.get('city')
+    country = location_data.get('country')
     
     params = {
         'chat_id': chat_id,
-        'text': 'User opened TheGates ' + user_id
+        'text': f'User opened TheGates {user_id} {city} {country}'
     }
 
     response = requests.get(url, params=params)
